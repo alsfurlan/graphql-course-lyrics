bfbe705c26bf2858e0f3d82cfe15fa17b3edf0b7
Anderson Luís Furlan <alsfurlan@gmail.com>
2023-11-06 18:13:18 -0300

    Handling pending queries

--- client/components/SongList.js
+++ client/components/SongList.js
@@ -3,9 +3,15 @@ import gql from "graphql-tag";
 import { graphql } from "react-apollo";
 
 class SongList extends Component {
+  renderSongs() {
+    return this.props.data.songs.map(({ title }) => <li>{title}</li>);
+  }
+
   render() {
-    console.log(this.props);
-    return <div>SongList</div>;
+    if (this.props.data.loading) {
+      return <div>Loading...</div>;
+    }
+    return <div>{this.renderSongs()}</div>;
   }
 }
 
