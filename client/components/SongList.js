8844038a6e47c8cdfcc371cdf621e75c7288e5f8
Anderson Luís Furlan <alsfurlan@gmail.com>
2023-11-09 18:57:52 -0300

    Navigating on successfull mutation

--- client/components/SongList.js
+++ client/components/SongList.js
@@ -1,19 +1,29 @@
-import React, { Component } from "react";
-import gql from "graphql-tag";
-import { graphql } from "react-apollo";
+import React, { Component } from 'react';
+import gql from 'graphql-tag';
+import { graphql } from 'react-apollo';
+import { Link } from 'react-router';
 
 class SongList extends Component {
   renderSongs() {
     return this.props.data.songs.map(({ id, title }) => (
-      <li key={id} className="collection-item">{title}</li>
+      <li key={id} className='collection-item'>
+        {title}
+      </li>
     ));
   }
 
   render() {
     if (this.props.data.loading) {
-      return <div>Loading...</div>;
+      return <div>Loading...</div>
     }
-    return <ul className="collection">{this.renderSongs()}</ul>;
+    return (
+      <div>
+        <ul className='collection'>{this.renderSongs()}</ul>
+        <Link to='/songs/new' className='btn-floating btn-large red right'>
+          <i className='material-icons'>add</i>
+        </Link>
+      </div>
+    );
   }
 }
 
