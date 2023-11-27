659ed5d2a3baeb458c1679855593138ba4f0ed55
Anderson Luís Furlan <alsfurlan@gmail.com>
2023-11-27 11:40:31 -0300

    Fetching individual records

--- client/components/SongDetail.js
+++ client/components/SongDetail.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
+import { graphql } from 'react-apollo';
+import fetchSong from '../queries/fetchSong';
 
 class SongDetail extends Component {
   render() {
+    console.log(this.props);
     return <h3>SongDetail</h3>;
   }
 }
 
-export default SongDetail;
+export default graphql(fetchSong, {
+  options: (props) => ({ variables: { id: props.params.id } }),
+})(SongDetail);
---
+++ client/queries/fetchSong.js
@@ -0,0 +1,11 @@
+import gpl from 'graphql-tag';
+
+export default gpl`
+    query SongQuery($id: ID!) {
+        song(id: $id) {
+            id
+            title
+        }
+    }
+
+`;
