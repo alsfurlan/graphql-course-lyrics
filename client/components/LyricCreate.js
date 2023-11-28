1f72b4a3eafb345bb57de4462c14ad1de02cd1fe
Anderson Luís Furlan <alsfurlan@gmail.com>
2023-11-28 16:59:46 -0300

    The CreateLyric mutation

--- client/components/LyricCreate.js
+++ client/components/LyricCreate.js
@@ -1,12 +1,42 @@
 import React, { Component } from 'react';
+import gpl from 'graphql-tag';
 
 export default class LyricCreate extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      content: '',
+    };
+  }
+
+  onSubmit(event) {
+    event.preventDefault();
+  }
+
   render() {
     return (
-      <form>
+      <form onSubmit={this.onSubmit.bind(this)}>
         <label>Add a Lyric</label>
-        <input />
+        <input
+          value={this.state.content}
+          onChange={(event) => this.setState({ content: event.target.value })}
+        />
       </form>
     );
   }
 }
+
+const mutation = gpl`
+mutation AddLyricToSong(
+  $content: String,
+  $songId: ID
+) {
+  addLyricToSong(content: $content, songId: $songId){
+  	id
+    lyrics {
+      id,
+      content
+    }
+  }
+}
+.;`;
