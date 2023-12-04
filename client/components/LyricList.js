ac3ae9c3524dc196f863d246495637601176da63
Anderson Luís Furlan <alsfurlan@gmail.com>
2023-12-04 19:00:03 -0300

    Enhancing queries

--- client/components/LyricList.js
+++ client/components/LyricList.js
@@ -1,8 +1,22 @@
 import React, { Component } from 'react';
 
-class LyricList extends Component{
+class LyricList extends Component {
+  constructor(props) {
+    super(props);
+  }
+
   render() {
-    return <ul>LyricList</ul>;
+    return <ul className='collection'>{this.renderLyrics()}</ul>;
+  }
+
+  renderLyrics() {
+    return this.props.lyrics.map(({ id, content }) => {
+      return (
+        <li key={id} className='collection-item'>
+          {content}
+        </li>
+      );
+    });
   }
 }
 
