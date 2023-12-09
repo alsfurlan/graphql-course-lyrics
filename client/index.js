325b10ed674e659a9f2fe5c767726f6b069126eb
Anderson Luís Furlan <alsfurlan@gmail.com>
2023-12-09 08:49:21 -0300

    Caching with DataIdFromObject

--- client/index.js
+++ client/index.js
@@ -1,28 +1,30 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import ApolloClient from "apollo-client";
-import { ApolloProvider } from "react-apollo";
-import App from "./components/App";
-import SongList from "./components/SongList";
-import { Router, Route, hashHistory, IndexRoute } from "react-router";
-import SongCreate from "./components/SongCreate";
-import SongDetail from "./components/SongDetail";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ApolloClient from 'apollo-client';
+import { ApolloProvider } from 'react-apollo';
+import App from './components/App';
+import SongList from './components/SongList';
+import { Router, Route, hashHistory, IndexRoute } from 'react-router';
+import SongCreate from './components/SongCreate';
+import SongDetail from './components/SongDetail';
 import './style/style.css';
 
-const client = new ApolloClient({});
+const client = new ApolloClient({
+  dataIdFromObject: (o) => o.id,
+});
 
 const Root = () => {
   return (
     <ApolloProvider client={client}>
       <Router history={hashHistory}>
-        <Route path="/" component={App}>
+        <Route path='/' component={App}>
           <IndexRoute component={SongList} />
-          <Route path="songs/new" component={SongCreate} />
-          <Route path="songs/:id" component={SongDetail} />
+          <Route path='songs/new' component={SongCreate} />
+          <Route path='songs/:id' component={SongDetail} />
         </Route>
       </Router>
     </ApolloProvider>
   );
 };
 
-ReactDOM.render(<Root />, document.querySelector("#root"));
+ReactDOM.render(<Root />, document.querySelector('#root'));
