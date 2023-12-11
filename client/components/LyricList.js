586a5ab767eaf65654f54a5876e2e6149840e672
Anderson Luís Furlan <alsfurlan@gmail.com>
2023-12-11 19:54:25 -0300

    Handling optimistic responses

--- client/components/LyricList.js
+++ client/components/LyricList.js
@@ -11,9 +11,17 @@ class LyricList extends Component {
     return <ul className='collection'>{this.renderLyrics()}</ul>;
   }
 
-  onLike(id) {
+  onLike(id, likes) {
     this.props.mutate({
       variables: { id },
+      optimisticResponse: {
+        __typename: 'Mutation',
+        likeLyric: {
+          __typename: 'LyricType',
+          id,
+          likes: ++likes,
+        }
+      }
     });
   }
 
@@ -23,7 +31,7 @@ class LyricList extends Component {
         <li key={id} className='collection-item'>
           {content}
           <div className="vote-box">
-            <i className='material-icons' onClick={() => this.onLike(id)}>
+            <i className='material-icons' onClick={() => this.onLike(id, likes)}>
               thumb_up
             </i>
             {likes}
