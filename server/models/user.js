c265091eaec4b54e6d68f6c6859a79568a469e8c
Anderson Luís Furlan <alsfurlan@gmail.com>
2024-01-09 11:11:25 -0300

    Create user model

---
+++ server/models/user.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+const { Schema, model } = mongoose;
+
+const UserSchema = new Schema({
+  email: String,
+  password: String,
+});
+
+module.exports = model('user', UserSchema);
